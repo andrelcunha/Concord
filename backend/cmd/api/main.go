3ea00acb2aedccb73d7b33b2cbe06c08b1584f0d
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2025-05-11 20:43:45 -0300

    feat: Implement backend application structure
    This commit introduces the basic structure for the backend application, including:
    
    - Project setup with `go.mod` and `go.sum` for dependency management.
    - Configuration loading using `godotenv` and a `config` package.
    - A `Makefile` for build and run commands.
    - A basic API setup using Fiber, including logging and a 404 handler.
    - Database and Redis initialization functions.
    - Initial authentication service setup (repository, service, and handler).
    - Addition of `.env` to `.gitignore` and a `bin/` directory.
    
    This commit lays the foundation for future backend development.

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/andrelcunha/Concord/backend/config"
+	"github.com/andrelcunha/Concord/backend/internal/auth"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	dbPool := initilizeDatabase()
+	defer dbPool.Close()
+
+	redisClient := initializeRedis()
+	defer redisClient.Close()
+
+	app := initializeFiber()
+
+	// Initialize auth service
+	authRepo := auth.NewRepository(dbPool)
+	authService := auth.NewService(authRepo, redisClient)
+	authHandler := auth.NewHandler(authService)
+
+	// Routes
+	addRoutes(app, authHandler)
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+func addRoutes(app *fiber.App, authHandler *auth.Handler) {
+	addCustom404Handler(app)
+}
+
+func initilizeDatabase() *pgxpool.Pool {
+	databaseURL := config.Config("DATABASE_URL")
+	dbPool, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v\n", err)
+	}
+	return dbPool
+}
+
+func initializeRedis() *redis.Client {
+	redisURL := config.Config("REDIS_URL")
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+	return redisClient
+}
+
+func initializeFiber() *fiber.App {
+	config := fiber.Config{
+
+		Prefork: false,
+		AppName: "Concord",
+		// Views:                 engine,
+		ViewsLayout: "layout",
+		// DisableStartupMessage: true,
+	}
+	app := fiber.New(config)
+	app.Use(logger.New(logger.Config{
+		Output: os.Stdout, // TODO: Change this to a logger
+	}))
+	return app
+}
+
+func addCustom404Handler(app *fiber.App) {
+	app.Use(func(ctx *fiber.Ctx) error {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"code":    fiber.StatusNotFound,
+			"message": "Route not found",
+		})
+	})
+}
